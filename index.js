f13a4ef3b137e5b6c869fed92f50f28da8f4cca0
jtk.eth <jesperkristensen58@users.noreply.github.com>
2023-06-23 23:24:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
         content: {
             /* Location of file where it is hosted */
             location: {
-                url: "https://github.com/panoptic-labs/litepaper/blob/8d78da798a8caf48cb2334e08be1e4b2a3e49ac7/Panoptic_Litepaper_v1.0.pdf",
+                url: "https://media.githubusercontent.com/media/panoptic-labs/litepaper/8d78da798a8caf48cb2334e08be1e4b2a3e49ac7/Panoptic_Litepaper_v1.0.pdf",
                 /*
                 If the file URL requires some additional headers, then it can be passed as follows:-
                 headers: [
