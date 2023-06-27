eec1ab1397dd802cec135ceff63b9fb3f7e3c8a9
jtk.eth <jesperkristensen58@users.noreply.github.com>
2023-06-26 21:15:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
         /* Pass meta data of file */
         metaData: {
             /* file name */
-            fileName: "Panoptic Litepaper v1.0.pdf"
+            fileName: "Panoptic Litepaper v1.1.pdf"
         }
     }, viewerConfig);
 });
